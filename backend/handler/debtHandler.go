18a792a68dcdd6b530de456459b34e32d46667aa
Alexander N. <alexnazim.811@gmail.com>
2026-07-12 22:55:58 +0300

    wip(request): реализовать взаимодействие с таблицей debts

---
+++ backend/handler/debtHandler.go
@@ -0,0 +1,97 @@
+package handler
+
+import (
+	"debtors/dto"
+	"debtors/service"
+	"encoding/json"
+	"net/http"
+)
+
+func (h *Handlers) CreateDebtHandler(w http.ResponseWriter, r *http.Request) {
+	debt := dto.DebtCreate{}
+
+	err := json.NewDecoder(r.Body).Decode(&debt)
+
+	if err != nil {
+		response := dto.Response{Status: "error", Data: err.Error()}
+		writeJSON(w, response)
+
+		return
+	}
+
+	resp, err := service.CreateDebtService(h.Conn, debt)
+
+	if err != nil {
+		response := dto.Response{Status: "error", Data: err.Error()}
+		writeJSON(w, response)
+
+		return
+	}
+
+	response := dto.Response{Status: "success", Data: resp}
+	writeJSON(w, response)
+}
+
+func (h *Handlers) ReadDebtHandler(w http.ResponseWriter, r *http.Request) {
+	resp, err := service.ReadDebtService(h.Conn)
+
+	if err != nil {
+		response := dto.Response{Status: "error", Data: err.Error()}
+		writeJSON(w, response)
+
+		return
+	}
+
+	response := dto.Response{Status: "success", Data: resp}
+	writeJSON(w, response)
+}
+
+func (h *Handlers) UpdateDebtHandler(w http.ResponseWriter, r *http.Request) {
+	debt := dto.Debt{}
+
+	err := json.NewDecoder(r.Body).Decode(&debt)
+
+	if err != nil {
+		response := dto.Response{Status: "error", Data: err.Error()}
+		writeJSON(w, response)
+
+		return
+	}
+
+	resp, err := service.UpdateDebtService(h.Conn, debt)
+
+	if err != nil {
+		response := dto.Response{Status: "error", Data: err.Error()}
+		writeJSON(w, response)
+
+		return
+	}
+
+	response := dto.Response{Status: "success", Data: resp}
+	writeJSON(w, response)
+}
+
+func (h *Handlers) DeleteDebtHandler(w http.ResponseWriter, r *http.Request) {
+	debt := dto.DebtDelete{}
+
+	err := json.NewDecoder(r.Body).Decode(&debt)
+
+	if err != nil {
+		response := dto.Response{Status: "error", Data: err.Error()}
+		writeJSON(w, response)
+
+		return
+	}
+
+	resp, err := service.DeleteDebtService(h.Conn, debt)
+
+	if err != nil {
+		response := dto.Response{Status: "error", Data: err.Error()}
+		writeJSON(w, response)
+
+		return
+	}
+
+	response := dto.Response{Status: "success", Data: resp}
+	writeJSON(w, response)
+}
--- backend/main.go
+++ backend/main.go
@@ -28,5 +28,10 @@ func main() {
 	http.HandleFunc("PUT /accounts", handler.UpdateAccountHandler)
 	http.HandleFunc("DELETE /accounts", handler.DeleteAccountHandler)
 
+	http.HandleFunc("POST /debts", handler.CreateDebtHandler)
+	http.HandleFunc("GET /debts", handler.ReadDebtHandler)
+	http.HandleFunc("PUT /debts", handler.UpdateDebtHandler)
+	http.HandleFunc("DELETE /debts", handler.DeleteDebtHandler)
+
 	http.ListenAndServe(":8080", nil)
 }
